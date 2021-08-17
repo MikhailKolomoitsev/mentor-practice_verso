d9301bc0ed7f7f88d5ca49d1c0f484e2bd5e473c
Wlad <wladkrychin@gmail.com>
2021-08-17 17:13:55 +0200

    in progress

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,31 +1,60 @@
-
+import s from './Footer.module.css'
 export default function Footer({link}) {
     return (
-        <footer>
+        <footer className={s.footer}>
             <a><svg>
                 <use href={link}/>
             </svg>
             </a>
-            <div>
-                <div>
-             <form>
-                    <label htmlFor="">
+            <div className={s.container}>
+                <div className={s.footer__content}>
+                    <form className={s.footer__form}>
+                        <label htmlFor="">
+                            Subscribe to our
+                            newsletter
+                                 </label>
                         <input type="text" placeholder="email"/>
-                    </label>
                 </form>
-                <ul>
-
+                    <ul>
+                        <li><a>Lorem Ipsum</a></li>
+                        <li><a>Lorem Ipsum</a></li>
+                        <li><a>Lorem Ipsum</a></li>
+                     <li><a>Lorem Ipsum</a></li>
                 </ul>
-                <ul>
+                    <ul>
+                          <li><a>Lorem Ipsum</a></li>
+                        <li><a>Lorem Ipsum</a></li>
+                        <li><a>Lorem Ipsum</a></li>
+                     <li><a>Lorem Ipsum</a></li>
 
                     </ul>
                 </div>
-                <ul>
-
+                <ul className={s.footer__socialList}>
+                  <li>
+                      <a href="">
+                          <svg>
+                              <use></use>
+                          </svg>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="">
+                          <svg>
+                              <use></use>
+                          </svg>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="">
+                          <svg>
+                              <use></use>
+                          </svg>
+                      </a>
+                  </li>
                 </ul>
-                <div>
-                    <p></p>
-                    <p></p>
+                <div className={s.footer__copyright}>
+                    <p>Copyright © 2021. Verso Network AG. All rights reserved.</p>
+                    <p>Privacy Policy</p>
                 </div>
                 
 </div>
