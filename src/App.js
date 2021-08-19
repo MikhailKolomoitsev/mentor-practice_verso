928642fceec956af262ab16c0b1ec4d6d4b62800
KasyanAndrey <andreykasyan88@gmail.com>
2021-08-19 03:01:27 +0300

    add files dataTable.json/Opportunities.jsx/Opportunities.module.css

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import Header from "./components/header/Header";
 import links from "./components/header/links.json";
 import { Hero } from "./components/hero/Hero.jsx";
+import Opportunities from "./components/section-opportunities/Opportunities";
+import dataTable from "./components/section-opportunities/dataTable.json";
 
 function App() {
   return (
     <div className="App">
       <Header linksArr={links} />
       <Hero />
+      <Opportunities items={dataTable} />
     </div>
   );
 }
