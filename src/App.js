ed6a61c372ba04cb242e97947063c21fc3a48580
Wlad <wladkrychin@gmail.com>
2021-08-24 14:05:27 +0200

    Пофиксил стили

--- src/App.js
+++ src/App.js
@@ -9,18 +9,15 @@ import ScrollButton from "./components/ScrollButton/ScrollButton";
 import Opportunities from "./components/section-opportunities/Opportunities";
 import dataTable from "./components/section-opportunities/dataTable.json";
 
-
 function App() {
   return (
     <div className="App">
       <Header linksArr={links} />
       <Hero />
-
-      <Footer />
       <ScrollButton />
 
       <Opportunities items={dataTable} />
-
+      <Footer />
     </div>
   );
 }
