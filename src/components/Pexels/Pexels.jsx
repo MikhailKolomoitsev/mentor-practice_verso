4bd28e8a28f81cb8a9e251fa7098529688b2b3ad
Wlad <wladkrychin@gmail.com>
2021-08-28 10:04:42 +0200

    fetch

--- src/components/Pexels/Pexels.jsx
+++ src/components/Pexels/Pexels.jsx
@@ -1,10 +1,11 @@
 import { Component } from "react";
 import axios from "axios";
 
-const KEY = "563492ad6f917000010000012505199bb27842618dc116a18c58462a";
+// const KEY = "563492ad6f91700001000001390f9fee0a794c1182a72e49e0e0eae2";
+const NEW_KEY = "563492ad6f917000010000013c1b42eecfa242ac8674f4d0dcb1a46e";
 const BASE_URL = "https://api.pexels.com/v1/search";
 
-axios.defaults.headers.common.Authorization = KEY;
+axios.defaults.headers.common.Authorization = NEW_KEY;
 axios.defaults.baseURL = BASE_URL;
 console.dir(axios);
 
@@ -12,6 +13,7 @@ export default class Pexels extends Component {
   state = {
     images: [],
     query: "",
+    page: 1,
   };
   handleChangeInput = (e) => {
     this.setState({ query: e.currentTarget.value });
@@ -26,13 +28,27 @@ export default class Pexels extends Component {
   //       .then((images) => this.setState({ images: [...images] }));
   //     this.setState({ query: "" });
   //   };
-  singleFormSubmit = async (e) => {
-    e.preventDefault();
-
-    let queryParams = `?query=${this.state.query}&per_page=5`;
+  componentDidUpdate(prevState, prevProps) {
+    if (prevState.page !== this.state.page) {
+      this.getFetch();
+    }
+  }
+  async getFetch() {
+    let queryParams = `?query=${this.state.query}&per_page=5&page=${this.state.page}`;
     const response = await axios.get(queryParams);
     const data = response.data.photos;
+    console.log(data);
     this.setState({ images: [...data] });
+  }
+  singleFormSubmit = (e) => {
+    e.preventDefault();
+
+    this.setState({ page: 1 });
+    this.getFetch();
+    this.setState({ query: "" });
+  };
+  handleClick = () => {
+    this.setState({ page: this.state.page + 1 });
   };
   render() {
     const { images, query } = this.state;
@@ -59,6 +75,9 @@ export default class Pexels extends Component {
               </li>
             ))}
         </ul>
+        <button type="button" onClick={this.handleClick}>
+          Load more
+        </button>
       </div>
     );
   }
