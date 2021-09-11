df8a2d0ceb7efb7bc4d4233275852fe4f820c3c9
Wlad <wladkrychin@gmail.com>
2021-09-11 08:25:02 +0200

    need to do styles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,14 +8,27 @@ import FooterForm from "./FooterForm";
 import FooterContent from "./FooterContent";
 import FooterLogo from "./FooterLogo";
 import Container from "../Container/Container";
+import { useState, useEffect } from "react";
 
 export default function Footer() {
+  const [value, setValue] = useState(
+    () => JSON.parse(localStorage.getItem("mail")) ?? "",
+  );
+
+  const handleFormSubmit = (query) => {
+    setValue(query);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("mail", JSON.stringify(value));
+  }, [value]);
+
   return (
     <footer className={s.footer}>
       <Container>
         <FooterLogo />
         <FooterContent>
-          <FooterForm />
+          <FooterForm submit={handleFormSubmit} />
           <FooterList items={items} />
           <FooterList items={items} />
         </FooterContent>
--- src/components/footer/FooterForm.jsx
+++ src/components/footer/FooterForm.jsx
@@ -1,12 +1,23 @@
 import { ReactComponent as ArrowRight } from "../../images/arrow-right.svg";
 import s from "./Footer.module.css";
-export default function FooterForm() {
+import { useState } from "react";
+export default function FooterForm({ submit }) {
+  const [mail, setMail] = useState("");
+
+  const handleInputChange = (e) => {
+    setMail(e.target.value);
+  };
+  const submitForm = (e) => {
+    e.preventDefault();
+    submit(mail);
+  };
   return (
-    <form className={s.footer__form}>
+    <form className={s.footer__form} onSubmit={submitForm}>
       <label htmlFor="footer-input" className={s.form__title}>
         Subscribe to our newsletter
       </label>
       <input
+        onChange={handleInputChange}
         type="text"
         placeholder="Email address"
         id="footer-input"
